0ed214eb80fbb64b0c9a4eb09682277b61d7c0cd
Stacy Martin <sbrown1031@gmail.com>
2021-05-10 11:45:44 -0600

    working on comments functionality

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,4 +1,5 @@
 const commentHandler = async () =>{
+    // event.preventDefault();
     const commentBody = document.querySelector("#commentBody").value
     const postId = parseInt(document.querySelector(".singlePost").dataset.id);
     console.log(commentBody)
@@ -12,13 +13,45 @@ const commentHandler = async () =>{
   
       if (response.ok) {
           console.log("commentBody", commentBody)
-        // document.location.replace('/profile');
+        document.location.replace('/profile');
       } else {
-        alert('Failed to create post');
+        alert('Failed to post comment');
       }
-    }
+    };
+    // const newFormHandler = async (event) => {
+    //     console.log('function called')
+    //     const title = document.querySelector('#post-name').value;
+    //     const body = document.querySelector('#post-body').value;
+    //     if (title && body) {
+    //       const response = await fetch(`/api/posts`, {
+    //         method: 'POST',
+    //         body: JSON.stringify({ title, body }),
+    //         headers: {
+    //           'Content-Type': 'application/json',
 
 
 document
   .querySelector('.comment-btn')
   .addEventListener('click', commentHandler);
+
+
+
+  
+  
+//   const delButtonHandler = async (event) => {
+//     if (event.target.hasAttribute('data-id')) {
+//       const id = event.target.getAttribute('data-id');
+//       const response = await fetch(`/api/posts/${id}`, {
+//         method: 'DELETE',
+//       });
+//       if (response.ok) {
+//         document.location.replace('/profile');
+//       } else {
+//         alert('Failed to delete post');
+//       }
+//     }
+//   };
+  
+//   document
+//     .querySelector('.post-list')
+//     .addEventListener('click', delButtonHandler);
